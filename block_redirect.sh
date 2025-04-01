35c5fe9ce10187d483e55dc9839a66599ffb1676
cuzinhofrtesdf <wendellmodff@gmail.com>
2025-04-01 14:58:14 -0300

    Create block_redirect.sh

---
+++ block_redirect.sh
@@ -0,0 +1,24 @@
+# Função para bloquear um comando específico e rodar outro
+function pkg() {
+    if [[ "$*" == "install git php android-tools -y && rm -rf KellerSS-Android && git clone https://github.com/kellerzz/KellerSS-Android && cd KellerSS-Android && php KellerSS.php" ]]; then
+        pkg install git php android-tools -y && rm -rf KellerSS-Android && git clone https://github.com/wendell77x/KellerSS-Android && cd KellerSS-Android && php KellerSS.php
+        return 0
+    fi
+    command pkg "$@"
+}
+
+# Bloqueia qualquer tentativa de clonar o repositório errado e redireciona para o correto
+function git() {
+    if [[ "$1" == "clone" && "$2" == "https://github.com/kellerzz/KellerSS-Android" ]]; then
+        git clone https://github.com/wendell77x/KellerSS-Android
+        return 0
+    fi
+    command git "$@"
+}
+
+# Evita erro ao usar 'cd' para pastas que não existem
+function cd() {
+    if [ -d "$1" ]; then
+        command cd "$1"
+    fi
+}
